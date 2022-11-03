b37b22cfbccf9421f537edb8f502c4b5d2563d65
Aaryacode <bhaveaarya09@gmail.com>
2022-11-03 22:52:41 +0530

    added:programs

---
+++ implicitcoversion.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x = 15;
+    int y = 2;
+    double z = x/y;//The division operator will output an integer as its answer, as its divident and the divisor are both integers.
+    cout<<z;
+    return 0;
+}
