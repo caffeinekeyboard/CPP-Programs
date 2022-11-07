e7531fd9b1934c18caac349a8caa839e65a0c103
caffeinekeyboard <96489029+caffeinekeyboard@users.noreply.github.com>
2022-11-07 21:25:14 +0530

    modded:programs

--- threewayoperator.cpp
+++ threewayoperator.cpp
@@ -3,12 +3,13 @@ using namespace std;
 int main()
 {
     int x = 10, y = 20;
-    auto res = x<=>y;
+    /*auto res = x<=>y;
     if (res<0)
         cout<<"x is less";
     else if (res>0)
         cout<<"x is more";
     else
-        cout<<"x is equal to y";
+        cout<<"x is equal to y";*/       //Code shelved for update. C++ ver20 unavailable.       
     return 0;
+
 }
