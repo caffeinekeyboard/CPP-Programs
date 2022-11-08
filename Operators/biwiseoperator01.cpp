86851f2765a2bc6731ca506c5010b73a0f2d02ba
caffeinekeyboard <96489029+caffeinekeyboard@users.noreply.github.com>
2022-11-08 14:08:09 +0530

    added:programs

---
+++ Operators/biwiseoperator01.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=3 , y=6;
+    cout<< (x&y) <<endl;//2
+    cout<< (x|y) <<endl;//7
+    cout<< (x^y) <<endl;//5
+    return 0;
+//Refer the notebook for detailed operator logic tables, and the detailed operation in each line.
+}
