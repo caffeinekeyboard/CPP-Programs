88ceaf84ea0daae181d2be7bed0f1065ddd1a628
caffeinekeyboard <96489029+caffeinekeyboard@users.noreply.github.com>
2022-11-12 14:15:24 +0530

    added:programs

--- Basics,IO,Variable&Datatypes/escapesequences.cpp
+++ Basics,IO,Variable&Datatypes/escapesequences.cpp
@@ -5,7 +5,7 @@ int main()
     string path = "C:\\Users\\Aarya\\Desktop\\CS>";//Will return C:\Users\Aarya\Desktop\CS>.
     cout<<path;
     //Similarly;
-    string gfg = "Welcome to \"GeeksforGeeks\"";//Will return Welcome to GeeksforGeeks.
+    string gfg = "Welcome to \"GeeksforGeeks\"";//Will return "Welcome to GeeksforGeeks".
     cout<<"\n"<<gfg;
     return 0;
 }
